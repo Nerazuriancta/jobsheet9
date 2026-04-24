0e217447a36099f25f183ae619d5562c6992070a
Nerazuriancta <nerasyahputri@gmail.com>
2026-04-24 22:31:15 +0700

    PERTANYAAN_PERCOBAAN2_JOBSHEET9

--- StackTugasMahasiswa19.java
+++ StackTugasMahasiswa19.java
@@ -73,4 +73,18 @@ public class StackTugasMahasiswa19 {
     public int count() {
         return top + 1;
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi19 stack = new StackKonversi19();
+        while (nilai != 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai=nilai/2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
